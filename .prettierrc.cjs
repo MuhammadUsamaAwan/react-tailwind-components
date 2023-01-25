116ca8bf27ea96cede2830a6ef8b7ac763650586
Muhammad Usama <underusama@gmail.com>
2023-01-25 09:49:37 +0500

    eslint prettier ts and tailwind setup

---
+++ .prettierrc.cjs
@@ -0,0 +1,12 @@
+module.exports = {
+  useTabs: false,
+  semi: true,
+  trailingComma: 'es5',
+  singleQuote: true,
+  printWidth: 120,
+  tabWidth: 2,
+  arrowParens: 'avoid',
+  jsxSingleQuote: true,
+  endOfLine: 'auto',
+  plugins: ['prettier-plugin-tailwindcss'],
+};
